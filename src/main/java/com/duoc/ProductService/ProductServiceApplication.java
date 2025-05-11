d90abd8d71895957f9e55951b8d4cc39131cd648
Franco Castro <franco.castro.villanueva.90@gmail.com>
2025-05-11 16:06:37 -0400

    config

--- src/main/java/com/duoc/ProductService/ProductServiceApplication.java
+++ src/main/java/com/duoc/ProductService/ProductServiceApplication.java
@@ -1,4 +1,4 @@
-package duoc.product_service;
+package com.duoc.ProductService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/duoc/ProductService/ProductServiceApplicationTests.java
+++ src/test/java/com/duoc/ProductService/ProductServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package duoc.product_service;
+package com.duoc.ProductService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
